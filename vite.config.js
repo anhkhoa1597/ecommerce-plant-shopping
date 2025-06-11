a88d474a810132205c3630e4190a14c9ce2d554a
Khoa Dang Anh <anhkhoa1597@yahoo.com.vn>
2025-06-11 13:11:50 +0700

    prepare to deploy

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/https://github.com/anhkhoa1597/ecommerce-plant-shopping/",
+  base: "/ecommerce-plant-shopping/",
 });
